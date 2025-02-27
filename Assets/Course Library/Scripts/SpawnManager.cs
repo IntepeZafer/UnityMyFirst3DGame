2fb913946b8549a659b5678db3da9731601198b4
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-02-27 22:45:44 +0300

    Update SpawnManager.cs

--- Assets/Course Library/Scripts/SpawnManager.cs
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public float spawnRangeX;
+    public float spawnPosZ;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex] , new Vector3(0 , 0 , 20) , animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
